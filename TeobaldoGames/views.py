5ee5620468c1040a7fe6d706ec3c1f6fa56a023e
willian <jcwillian.santos@gmail.com>
2014-11-01 15:11:32 -0200

    renderizando o templete home

--- TeobaldoGames/views.py
+++ TeobaldoGames/views.py
@@ -4,4 +4,4 @@ from flask import render_template
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('base.html')
+    return render_template('home.html', title='Home - TeobaldoGames')
