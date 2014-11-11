6441448f196d851e4e029e97e6ffad6c2438aa70
will <jcwillian.santos@gmail.com>
2014-11-11 11:22:26 -0200

    import and route for login

--- TeobaldoGames/views.py
+++ TeobaldoGames/views.py
@@ -1,7 +1,18 @@
 from TeobaldoGames import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title='Home - TeobaldoGames')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login sucessu.')
+		return redirect('/home')
+	return render_template('login.html', 
+							title='Sign In',
+							form = form)
