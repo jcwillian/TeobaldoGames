d6e71b313791caea9ada4d9b189c563c1ec51d42
will <jcwillian.santos@gmail.com>
2014-11-12 21:35:07 -0200

    add formulario para cadastro usuario

--- TeobaldoGames/forms.py
+++ TeobaldoGames/forms.py
@@ -5,3 +5,9 @@ from wtforms.validators import DataRequired
 class LoginForm(Form):
 	username = StringField('username', validators=[DataRequired()])
 	password = StringField('password', validators=[DataRequired()])
+
+class CadastroForm(Form):
+	nomeUsuario = StringField('nomeUsuario', validators=[DataRequired()])
+	username = StringField('username', validators=[DataRequired()])
+	email = StringField('emal', validators=[DataRequired()])
+	senha = StringField('senha', validators=[DataRequired()])
