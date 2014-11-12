55a9a3278cdc5207c8780ba120343afa3d3478d4
will <jcwillian.santos@gmail.com>
2014-11-12 09:46:58 -0200

    Instanciando SQLAlchemy e importando models

--- TeobaldoGames/__init__.py
+++ TeobaldoGames/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask 
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask('TeobaldoGames')
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from TeobaldoGames import views
+from TeobaldoGames import views, models
