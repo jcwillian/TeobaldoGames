4c14a42415f0c2726525c133fd3976d96c14a897
will <jcwillian.santos@gmail.com>
2014-11-12 21:33:58 -0200

    add LoginManager and configurando login_view para login

--- TeobaldoGames/__init__.py
+++ TeobaldoGames/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask 
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
 app = Flask('TeobaldoGames')
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 from TeobaldoGames import views, models
