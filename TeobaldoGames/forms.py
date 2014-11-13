74464542d14419de3ab11ca655a7c77e1697b800
will <jcwillian.santos@gmail.com>
2014-11-12 22:41:35 -0200

    mudando campo de Senha, e validando email

--- TeobaldoGames/forms.py
+++ TeobaldoGames/forms.py
@@ -1,13 +1,13 @@
-from flask.ext.wtf import Form 
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from flask_wtf import Form 
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email
 
 class LoginForm(Form):
 	username = StringField('username', validators=[DataRequired()])
-	password = StringField('password', validators=[DataRequired()])
+	password = PasswordField('password', validators=[DataRequired()])
 
 class CadastroForm(Form):
 	nomeUsuario = StringField('nomeUsuario', validators=[DataRequired()])
 	username = StringField('username', validators=[DataRequired()])
-	email = StringField('emal', validators=[DataRequired()])
-	senha = StringField('senha', validators=[DataRequired()])
+	email = StringField('emal', validators=[Email(message='Email invalido')])
+	senha = PasswordField('senha', validators=[DataRequired()])
