24554584c351ff8bf6a973854f78a99ad6121372
will <jcwillian.santos@gmail.com>
2014-11-16 10:39:00 -0200

    afd

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 UPLOAD_FOLDER = os.path.join(basedir, 'uploads_images')
