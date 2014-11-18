e2a7df089b2fe5582c847998f4f7ef3cfa2a967b
will <jcwillian.santos@gmail.com>
2014-11-18 10:50:54 -0200

    mudando campo de foto

--- TeobaldoGames/forms.py
+++ TeobaldoGames/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form 
-from wtforms import StringField, PasswordField, TextAreaField, FloatField, FileField, IntegerField
+from wtforms import StringField, PasswordField, TextAreaField, FloatField, StringField, IntegerField
 from wtforms.validators import DataRequired, Email
 
 class LoginForm(Form):
@@ -7,7 +7,7 @@ class LoginForm(Form):
 	password = PasswordField('password', validators=[DataRequired()])
 
 class CadastroForm(Form):
-	photo = FileField('photo')
+	photo = StringField('photo')
 	name = StringField('name', validators=[DataRequired()])
 	nickname = StringField('nickname', validators=[DataRequired()])
 	email = StringField('email', validators=[Email(message='Email invalido')])
@@ -17,7 +17,7 @@ class CadastroForm(Form):
 class AddDetonadoForm(Form):
 	title = StringField('title', validators=[DataRequired()])
 	body = TextAreaField('body')
-	photo = FileField('photo')
+	photo = StringField('photo')
 
 class FeedbackForm(Form):
 	message = TextAreaField('message')
@@ -26,7 +26,7 @@ class GameForm(Form):
 	name = StringField('name', validators=[DataRequired()])
 	description = TextAreaField('description')
 	price = FloatField('price', validators=[DataRequired()])
-	photo_game = FileField('photo_game')
+	photo_game = StringField('photo_game')
 
 class SearchForm(Form):
 	words = StringField('words')
@@ -39,11 +39,11 @@ class EditePerfilForm(Form):
 	email = StringField('email', validators=[Email(message='Email invalido')])
 
 class AtualizePhotoForm(Form):
-	photo = FileField('photo', validators=[DataRequired()])
+	photo = StringField('photo', validators=[DataRequired()])
 
 class AtualizeGameForm(Form):
 	name = StringField('name')
 	description = TextAreaField('description')
 	price = FloatField('price')
-	photo_game = FileField('photo_game')
+	photo_game = StringField('photo_game')
 	
